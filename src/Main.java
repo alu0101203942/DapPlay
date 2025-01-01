d1664923709b22f89dbb30841299e40ebe3cb24f
Laura González <alu0101203942@ull.edu.es>
2025-01-01 22:26:36 +0000

    Eliminando un warning

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         SteamApiService service = new SteamApiService("06166564FA99EDCBCEDAFFF71732218B");
         MainView view = new MainView();
-        MainController controller = new MainController(service, view);
+        new MainController(service, view);
         view.show();
     }
 }
