c7f2b8f12b7f2da0a85d129b02044c9764fd5e79
Laura González <alu0101203942@ull.edu.es>
2025-01-01 22:00:12 +0000

    Implementando la GUI en MainView y empezando a definir el controlador

--- src/Controlador/MainController.java
+++ src/Controlador/MainController.java
@@ -1,4 +1,15 @@
 package src.Controlador;
 
+import src.Modelo.SteamApiService;
+import src.Vista.MainView;
+
+
 public class MainController {
+    private final SteamApiService steamApiService;
+    private final MainView mainView;
+
+    public MainController(SteamApiService steamApiService, MainView mainView) {
+        this.steamApiService = steamApiService;
+        this.mainView = mainView;
+    }
 }
