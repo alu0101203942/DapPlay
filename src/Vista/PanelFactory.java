bb2d3aa151511b2e5199d997e6af89937f8d31b9
Laura González <alu0101203942@ull.edu.es>
2025-01-02 18:12:18 +0000

    definiendo los métodos para el panel de juegos y el de favoritos

--- src/Vista/PanelFactory.java
+++ src/Vista/PanelFactory.java
@@ -1,5 +1,27 @@
 package src.Vista;
 
+import com.lukaspradel.steamapi.data.json.ownedgames.Game;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+
 public class PanelFactory {
+    public static JPanel createGamePanel(Game game, ActionListener favoriteAction) {
+        JPanel gamePanel = new JPanel(new BorderLayout());
+        gamePanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+
+        return gamePanel;
+    }
+    public static JPanel createFavoritePanel(Game game, ActionListener removeAction) {
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+
+        return panel;
+    }
 
 }
